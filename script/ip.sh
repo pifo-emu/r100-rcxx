ab9a394f82c0d71b1ad12465d68694cf350d0aa0
pifo-emu <jc349563@gmail.com>
2021-12-30 12:39:22 +0100

    update

--- script/ip.sh
+++ script/ip.sh
@@ -1,7 +1,4 @@
 rm /tmp/lo /tmp/GeoIP-legacy.csv > /dev/null 2>&1
-echo "DIME LA RUTA DE TU IP2COUNTRY"
-read -p "Enter La ruta: " -r primary_domain
-read -p "Enter Nombre de tu IP2COUNTRY: " -r primary_domain2
 /usr/bin/wget -q "https://mailfud.org/geoip-legacy/GeoIP-legacy.csv.gz" -O  /tmp/GeoIP-legacy.csv.gz
 gzip -d /tmp/GeoIP-legacy.csv.gz
 echo "###############################################################
@@ -10,6 +7,6 @@ echo "###############################################################
 #              LAST EDITION echo `date +%d-%m-%Y` BY PIFO	      #
 ###############################################################
 " > /tmp/lo
-cat /tmp/lo /tmp/GeoIP-legacy.csv > $primary_domain/$primary_domain2
+cat /tmp/lo /tmp/GeoIP-legacy.csv > /usr/local/etc/multics/FILE/ip2country.csv
 echo Update completed 
 exit 0
